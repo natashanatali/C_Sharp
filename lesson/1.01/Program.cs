98667929870aeb452b56d224001f0bcdb9026769
natali <natasha.sotova1@mail.ru>
2022-11-25 18:28:08 +0300

    task1_01

---
+++ lesson/1.01/Program.cs
@@ -0,0 +1,16 @@
+﻿
+Console.WriteLine("Ведите целое число1");
+int num1 = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Ведите целое число2");
+int num2 = int.Parse(Console.ReadLine());
+
+if(num2 == num1 * num1)
+{
+    Console.WriteLine("да");
+}
+
+else
+    Console.WriteLine("нет");
+
+
