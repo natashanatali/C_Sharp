d0ff420b7043656dd99125fab646dd891b071787
natali <natasha.sotova1@mail.ru>
2022-11-25 18:51:19 +0300

    task1_02

---
+++ lesson/1.02/Program.cs
@@ -0,0 +1,35 @@
+﻿
+Console.WriteLine("Введите число");
+int y = int.Parse(Console.ReadLine());
+if (y == 1)
+{
+    Console.WriteLine("понедельник");
+}
+else if (y == 2)
+{
+    Console.WriteLine("вторник");
+}
+else if (y == 3)
+{
+    Console.WriteLine("среда");
+}
+else if (y == 4)
+{
+    Console.WriteLine("четверг");
+}
+else if (y == 5)
+{
+    Console.WriteLine("пятница");
+}
+else if (y == 6)
+{
+    Console.WriteLine("суббота");
+}
+else if (y == 7)
+{
+    Console.WriteLine("воскресенье");
+}
+else 
+{
+    Console.WriteLine("error");
+}
