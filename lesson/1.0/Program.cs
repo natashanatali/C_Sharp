dc9af846345ca0f04564c42e43cd3c8114a5f0aa
natali <natasha.sotova1@mail.ru>
2022-11-25 17:52:21 +0300

    task1_0

--- lesson/1.0/Program.cs
+++ lesson/1.0/Program.cs
@@ -1,2 +1,5 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+Console.WriteLine("Write a number: ");
+
+int num = int.Parse(Console.ReadLine());
+Console.WriteLine(num * num);
