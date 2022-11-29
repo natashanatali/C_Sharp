e1941a3794aebb07c480204c173eaed534aba0e7
natali <natasha.sotova1@mail.ru>
2022-11-29 22:06:50 +0300

    task 2.0

---
+++ lesson2/2.0/Program.cs
@@ -0,0 +1,12 @@
+﻿// напишите программу , которая вводит случайное число из отрезка (10,99) и показывает наибольшую цифру числа.
+
+int MaxNumber(int num)
+{
+    Console.WriteLine(num);
+
+    if (num %10 > num / 10)
+        return num % 10;
+    else
+        return num / 10;    
+}
+Console.WriteLine(MaxNumber(new Random().Next(10, 100)));
