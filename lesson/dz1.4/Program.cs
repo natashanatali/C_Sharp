7ee7aa3cbc650eab36fe16e777be2f49fd976051
natali <natasha.sotova1@mail.ru>
2022-11-29 19:56:59 +0300

    dz 1.4

---
+++ lesson/dz1.4/Program.cs
@@ -0,0 +1,14 @@
+﻿//  Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+Console.WriteLine("Введите число:");
+int num = int.Parse(Console.ReadLine());
+int count = 2;
+if (num == 1)
+{
+    Console.WriteLine("Четных чисел нет");
+}
+while (count <= num)
+{
+    Console.Write($"{count} ");
+    count +=2;
+}
+Console.WriteLine();
