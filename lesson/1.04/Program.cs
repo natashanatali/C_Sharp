586930efd407b6db706cb13b0cc78b0cd2bde4bb
natali <natasha.sotova1@mail.ru>
2022-11-29 16:53:47 +0300

    task 1.04

---
+++ lesson/1.04/Program.cs
@@ -0,0 +1,8 @@
+﻿// напишите программу вычисления модуля чисел -6->6
+
+Console.Write("Ведите число:");
+var S = Console.ReadLine();
+int N = S == null ? 0 :int.Parse(S);
+Console.WriteLine($"{Math.Abs(N)}");
+
+
