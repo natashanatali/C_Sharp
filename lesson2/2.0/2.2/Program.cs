ba55d10dc9581a578cc8b8d4059d807ee3f6e820
natali <natasha.sotova1@mail.ru>
2022-11-29 23:04:31 +0300

    task 2.2

---
+++ lesson2/2.0/2.2/Program.cs
@@ -0,0 +1,15 @@
+﻿// 2.2
+string crat(int num, int num2)
+
+{
+    if(num % num2 == 0)
+        return "кратно";
+    else 
+        return $"не кратно, остаток = {num % num2}";  
+}
+Console.WriteLine("1 chislo");
+int first = int.Parse(Console.ReadLine());
+Console.WriteLine("2 chislo");
+int second = int.Parse(Console.ReadLine());
+Console.WriteLine(crat(first, second));
+
