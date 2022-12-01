bd183a6ddf11e24c4d2ef9d99674b96139d7a03e
natali <natasha.sotova1@mail.ru>
2022-12-01 20:59:18 +0300

    dz2.2

---
+++ lesson2/dz2.2/Program.cs
@@ -0,0 +1,11 @@
+﻿// Напишите программу, которая принимает на вход трёхзначное число
+// и на выходе показывает вторую цифру этого числа.
+
+Console.WriteLine("Введите трехзначное число:");
+string Nums(int num)
+{
+    if ((-1000 < num && num <-100) || (num < 1000 && num > 100))
+        return $"{num / 10 % 10}";
+    return "цифра не трехзначная";    
+}
+    Console.WriteLine(Nums(int.Parse(Console.ReadLine())));
