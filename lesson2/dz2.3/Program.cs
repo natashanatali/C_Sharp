b5270ebc8241415096ad4604c3aee32d3f878a3c
natali <natasha.sotova1@mail.ru>
2022-12-01 20:24:51 +0300

    dz2.3

---
+++ lesson2/dz2.3/Program.cs
@@ -0,0 +1,14 @@
+﻿// Напишите программу, которая принимает на вход цифру,
+//обозначающую день недели, и проверяет, является ли этот день выходным.
+
+string Week(int data)
+{
+    if (data == 6 || data == 7)
+        return "выходной день";
+    else
+        return "будний день";           
+}
+
+Console.WriteLine("Введите номер:");
+int num = int.Parse(Console.ReadLine());
+Console.WriteLine(Week(num));
