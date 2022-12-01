bf120168cd05d8864980af529eaebe7d96bbcecb
natali <natasha.sotova1@mail.ru>
2022-12-01 20:08:07 +0300

    dz2.0

---
+++ lesson2/dz2.0/Program.cs
@@ -0,0 +1,24 @@
+﻿//Напишите программу, которая выводит третью цифру
+//заданного числа или сообщает, что третьей цифры нет.
+
+
+string Number(int n)
+{
+    if (n < 100)
+        return "not have third number";
+    else if (n > 99 && n < 1000)
+        return $"Number is {n % 10}"; 
+    else
+    {
+        while (n > 1000)
+        {
+            n = n / 10;
+        }   
+    }
+        return $"Number is {n % 10}";  
+
+}
+
+Console.WriteLine("Input number:");
+int num = int.Parse(Console.ReadLine());
+Console.WriteLine(Number(num));
