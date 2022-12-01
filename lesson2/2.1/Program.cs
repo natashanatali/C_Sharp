20fabfcabcd39483434db681eeb258a4d7764e4a
natali <natasha.sotova1@mail.ru>
2022-12-01 19:17:51 +0300

    2.3

---
+++ lesson2/2.1/Program.cs
@@ -0,0 +1,9 @@
+﻿// напишите программу,которая выводит случайное число трехзначное ,и на выходе покаывает последнюю цифру этого числа.
+
+int Num(int num)
+{
+   Console.WriteLine(num);
+
+    return num %10;
+}
+Console.WriteLine(Num(new Random().Next(100,1000)));
---
+++ lesson2/2.2/Program.cs
@@ -0,0 +1,13 @@
+﻿
+string Crat(int num, int num2)
+{
+    if(num % num2 == 0)
+       return "кратно";
+    else 
+       return $"некратно, остаток = {num % num2}"; 
+}
+Console.WriteLine("1 chislo");
+int first = int.Parse(Console.ReadLine());
+Console.WriteLine("2 chislo");
+int second = int.Parse(Console.ReadLine());
+Console.WriteLine(Crat(first, second));
