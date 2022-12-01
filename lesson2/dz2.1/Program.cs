a99f99b4be7ef8309c6ba719be7680c3a61f7c7d
natali <natasha.sotova1@mail.ru>
2022-12-01 20:11:50 +0300

    dz2.1

---
+++ lesson2/dz2.1/Program.cs
@@ -0,0 +1,12 @@
+﻿// Напишите программу, которая выводит случайное
+//трёхзначное число и удаляет вторую цифру этого числа
+int Num(int n)
+{
+    Console.WriteLine($"Our number: {n}");
+    int ost = n % 10;
+    n = n / 100;
+    int num = n * 10 + ost;
+    return num;
+}
+
+Console.WriteLine(Num(new Random().Next(100,1000)));
