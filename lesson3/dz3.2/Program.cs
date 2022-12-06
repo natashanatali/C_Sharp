f8a97ee7bceaca070b81c8822f9b26f898bc3916
natali <natasha.sotova1@mail.ru>
2022-12-07 01:15:04 +0300

    dz3.3

---
+++ lesson3/dz3.2/Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+double Linia(int ax, int ay, int az, int bx, int by, int bz)
+{
+  return Math.Round(Math.Sqrt(Math.Pow(bx - ax, 2) + Math.Pow(by - ay, 2) + Math.Pow(bz - az, 2)),2);
+  
+}
+Console.WriteLine(Linia(5, 1, 7, -4, 6, 8));
---
+++ lesson3/dz3.3/Program.cs
@@ -0,0 +1,25 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+void Tabl(int n)
+{
+    int count = 1;
+    while (count <= n)
+        {
+            if (count == n)
+            {
+                double kub = Math.Pow(count,3);
+                Console.Write($"{kub}");
+                Console.WriteLine();
+            }
+            else
+            {
+                double kub = Math.Pow(count,3);
+                Console.Write($"{kub}, ");
+            }
+
+            count +=1;
+        }
+}
+Console.WriteLine("Введите число:");
+Tabl(int.Parse(Console.ReadLine()));
