4c37ab43650c5ce39fa130dc5b46e1655adeb83b
natali <natasha.sotova1@mail.ru>
2022-12-07 00:39:00 +0300

    dz3.1

---
+++ lesson3/dz3.1/Program.cs
@@ -0,0 +1,25 @@
+﻿//Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом
+
+string Polindrom(string str)
+{
+    int n = str.Length;
+    int count = 0;
+    int half = n / 2;
+    while (count < half)
+    {
+        if (str[count] == str[n-1])
+        {
+            n--; 
+            count++;      
+        }  
+        else
+            break;
+    }
+    if (half == count)
+        return "Полиндром";
+    else
+        return "Не полиндром";
+}
+
+Console.WriteLine("Bведите пятизначное число:");
+Console.WriteLine(Polindrom(Console.ReadLine()));
