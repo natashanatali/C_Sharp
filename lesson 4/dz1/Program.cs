d927730032ead4451d2af9bc2f8f07cbb684c488
natali <natasha.sotova1@mail.ru>
2022-12-09 14:09:00 +0300

    dz1

---
+++ lesson 4/dz1/Program.cs
@@ -0,0 +1,20 @@
+﻿// Напишите цикл, который принимает на вход два числа (A и B) 
+// и возводит число A в натуральную степень B.
+
+int Stepen(int A, int B)
+{
+    int x = 1;
+
+    for (int i = 1; i <= B; i++)
+    {
+        x = x * A;
+    }
+    return x;
+}
+
+Console.WriteLine("Введите число1: ");
+int num1 = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите число2: ");
+int num2 = int.Parse(Console.ReadLine());
+
+Console.WriteLine(Stepen(num1, num2));
