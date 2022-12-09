d386f1f810c86d2cd2e4f688db89100336294bdb
natali <natasha.sotova1@mail.ru>
2022-12-09 14:11:24 +0300

    dz2

---
+++ lesson 4/dz2/Program.cs
@@ -0,0 +1,20 @@
+﻿// Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+
+int SumNums(int num)
+{
+    int sum = 0;
+    int new_num = 0;
+
+    while (num > 0)
+    {
+        new_num = num % 10;
+        sum = sum + new_num;
+        num = num / 10;
+    }
+    return sum;
+}
+
+Console.WriteLine("Введите число: ");
+int n = int.Parse(Console.ReadLine());
+
+Console.WriteLine(SumNums(n));
