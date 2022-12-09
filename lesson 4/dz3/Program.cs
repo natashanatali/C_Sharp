b4f5b206ca65aae1481c2771814e889215e07d05
natali <natasha.sotova1@mail.ru>
2022-12-09 14:15:09 +0300

    dz3

---
+++ lesson 4/dz3/Program.cs
@@ -0,0 +1,22 @@
+﻿// Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран
+
+void FillArray(int[] massiv)
+{
+    for (int i = 0; i < massiv.Length; i++)
+    {
+        massiv[i] = new Random().Next(1, 100);
+    }
+}
+
+void PrintArray(int[] mas)
+{
+    for (int i = 0; i < mas.Length; i++)
+    {
+        Console.WriteLine(mas[i]);
+    }
+}
+
+Console.Write("Введите число элементов массива: ");
+int[] array = new int[int.Parse(Console.ReadLine())];
+FillArray(array);
+PrintArray(array);
