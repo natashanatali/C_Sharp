03a3f4b6eb20aee574b7694fe6c2d768c1b1cbc0
natali <natasha.sotova1@mail.ru>
2022-12-19 14:40:34 +0300

    dz

---
+++ lesson 5/dz1/Program.cs
@@ -0,0 +1,46 @@
+﻿// Задайте массив заполненный случайными положительными трёхзначными числами. Напишите
+// программу, которая покажет количество чётных чисел в массиве.
+
+void Print(int[] arr)
+{
+    int size = arr.Length;
+
+    for (int i = 0; i < size; i++)
+    {
+        Console.Write($"{arr[i]} ");
+    }
+    Console.WriteLine();
+}
+
+int[] MassNums(int size, int from, int to)
+{
+    int[] arr = new int[size];
+
+    for (int i = 0; i < size; i++)
+    {
+        arr[i] = new Random().Next(from, to);
+    }
+    return arr;
+}
+
+int EvenNum(int[] arr)
+{
+    int count = 0;
+
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (arr[i] % 2 == 0)
+        {
+            count += 1;
+        }
+    }
+    return count;
+}
+
+int[] arr_1 = MassNums(15, 100, 251);
+Print(arr_1);
+Console.WriteLine(EvenNum(arr_1));
+
+int[] arr_2 = MassNums(6, 100, 251);
+Print(arr_2);
+Console.WriteLine(EvenNum(arr_2));
---
+++ lesson 5/dz2/Program.cs
@@ -0,0 +1,44 @@
+﻿/// 2. Задайте одномерный массив, заполненный случайными числами.
+//    Найдите сумму элементов, стоящих на нечётных позициях. 
+
+
+void Print(int[] arr)
+{
+    int size = arr.Length;
+
+    for (int i = 0; i < size; i++)
+    {
+        Console.Write($"{arr[i]} ");
+    }
+    Console.WriteLine();
+}
+
+int[] MassNums(int size, int from, int to)
+{
+    int[] arr = new int[size];
+
+    for (int i = 1; i < size; i++)
+    {
+        arr[i] = new Random().Next(from, to);
+    }
+    return arr;
+}
+
+int OddNumSum(int[] arr)
+{
+    int count = 0;
+
+    for (int i = 0; i < arr.Length; i += 2)
+    {
+        count += arr[i];
+    }
+    return count;
+}
+
+int[] arr_1 = MassNums(10, 1, 11);
+Print(arr_1);
+Console.WriteLine(OddNumSum(arr_1));
+
+int[] arr_2 = MassNums(4, 1, 11);
+Print(arr_2);
+Console.WriteLine(OddNumSum(arr_2));
