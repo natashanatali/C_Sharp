78cb83824bc66db180fcab1d876b356f652156cd
natali <natasha.sotova1@mail.ru>
2022-12-21 13:27:25 +0300

    dz

---
+++ lesson 6/dz 2/Program.cs
@@ -0,0 +1,22 @@
+﻿// : Напишите программу, которая найдёт точку пересечения двух прямых, заданных уравнениями 
+//y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
+//b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
+
+void Intersection(double b1, double k1, double b2, double k2)
+{
+    double x = (b2 - b1) / (k1 - k2);
+    double y = k1 * x + b1;
+    Console.WriteLine($"Intersection point: ({x}; {y})");
+}
+
+Console.Write("Введите b1: ");
+double b1 = double.Parse(Console.ReadLine());
+Console.Write("Введите k1: ");
+double k1 = double.Parse(Console.ReadLine());
+Console.Write("Введите b2: ");
+double b2 = double.Parse(Console.ReadLine());
+Console.Write("Введите k2: ");
+double k2 = double.Parse(Console.ReadLine());
+Intersection(b1, k1, b2, k2);
+
+
