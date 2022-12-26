ad3b0425a070879c10e8787e2b0f2db23cd13789
natali <natasha.sotova1@mail.ru>
2022-12-26 22:38:50 +0300

    9lesson

---
+++ lesson 9/dz 1/Program.cs
@@ -0,0 +1,14 @@
+﻿//  Задайте значения M и N. Напишите программу, которая выведет все чётные натуральные числа в промежутке от M до N.
+
+
+void NaturNum(int m, int n)
+{
+    if (n % 2 != 0)
+        n = n - 1;
+    if (n < m) return;
+    NaturNum(m, n - 2);
+    Console.Write($" {n} ");
+}
+
+NaturNum(1, 5);
+NaturNum(4, 8);
